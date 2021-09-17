65891773c0dc8d2db4cb696a6bbfb83cdd1bef69
uventura <ualivent@gmail.com>
2021-09-17 16:57:07 -0300

    Musicas do Youtube

--- index.py
+++ index.py
@@ -5,15 +5,16 @@ import json
 import requests
 import youtube_dl
 import os
+
 from decouple import config
 
 client = discord.Client()
-TOKEN = config('TOKEN')
-    
+TOKEN = os.environ['TOKEN'] # config('TOKEN') -> se estiver no computador
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -21,36 +22,43 @@ async def on_message(message):
         return
 
     channel = message.channel
-    if ('sena') in message.content:
-        url="https://www.youtube.com/watch?v=p37_Ux1G_BI"
-        song_there = os.path.isfile("song.mp3")
-        try:
-            if song_there:
-                os.remove("song.mp3")
-        except PermissionError:
-            await channel.send("Wait for the current playing music to end or use the 'stop' command")
-            return
+    if '-play' in message.content:
+        command = message.content.split()
+        print(len(command)!=2)
+        if(len(command)!=2):
+          await message.channel.send('Digita o comando certo seu bocó')
+        else:
+          url = command[1]
+          song_there = os.path.isfile("song.mp3")
+          try:
+              if song_there:
+                  os.remove("song.mp3")
+          except PermissionError:
+              await channel.send(
+                  "Wait for the current playing music to end or use the 'stop' command"
+              )
+              return
 
-        channel = message.author.voice.channel
-        await channel.connect()
-        voice = discord.utils.get(client.voice_clients, guild=channel.guild)
+          channel = message.author.voice.channel
+          voice = await channel.connect()
 
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        for file in os.listdir("./"):
-            if file.endswith(".mp3"):
-                os.rename(file, "song.mp3")
-        voice.play(discord.FFmpegPCMAudio("song.mp3"))
-        await channel.send('fat and gay')
-
+          ydl_opts = {
+              'format':
+              'bestaudio/best',
+              'postprocessors': [{
+                  'key': 'FFmpegExtractAudio',
+                  'preferredcodec': 'mp3',
+                  'preferredquality': '192',
+              }],
+          }
+          with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+              ydl.download([url])
+        
+          for file in os.listdir("./"):
+              if file.endswith("mp3"):
+                  os.rename(file, "song.mp3")
+          voice.play(
+              discord.FFmpegPCMAudio(source="song.mp3"))
 
 
 #Play
@@ -63,8 +71,7 @@ async def on_message(message):
     elif (message.author.voice) and message.content.startswith('-leave'):
         await channel.send("Forte Abraço!!")
         await channel.guild.voice_client.disconnect()
-    elif message.author.voice == None:
-        await channel.send("Tu não ta on no voice, macaco")
-
+    elif message.author.voice == None and message.content.startswith('-leave'):
+        await channel.send("Tu não ta on no voice")
 
 client.run(TOKEN)
