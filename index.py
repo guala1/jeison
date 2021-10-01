343a7c712639c2f047a81e15d6eb5646b390fe09
Gustavo Policena <90911885+guala1@users.noreply.github.com>
2021-10-01 16:12:00 +0000

    plotagem e equação de 1º grau

--- index.py
+++ index.py
@@ -1,17 +1,19 @@
 import discord
-from discord.ext import commands
-from discord import FFmpegPCMAudio
-import json
-import requests
+#from discord.ext import commands
+#from discord import FFmpegPCMAudio
+#import json
+#import requests
 import youtube_dl
 import os
 
-from decouple import config
-client = discord.Client()
-TOKEN = os.environ['TOKEN'] # config('TOKEN') -> se estiver no computador
+import requests, json
+import pyimgur
 
-voice = None
-songsList = []
+from math import *
+
+#from decouple import config
+from random import randint
+import matplotlib.pyplot as plt
 
 def playSong():
   global voice
@@ -35,6 +37,45 @@ def playSong():
             os.rename(file, "song.mp3")
     voice.play(
         discord.FFmpegPCMAudio(source="song.mp3"))
+
+def getPhoto(name, url):
+  # https://i.imgur.com/
+  # Open HTML File
+  html = open(name+'.html', 'r').read()
+
+  urls=[]
+  i = 0
+  current = ''
+  addElement = False
+  while i<len(html):
+      if (html[i]=='h' and i+len('ref="'+url)<len(html) and html[i:i+len('ref="'+url)+1]=='href="'+url):
+          addElement = True
+          i += len('ref="'+url)
+      elif addElement and html[i]!='"':
+          current+=html[i]
+      elif addElement and html[i]=='"':
+        if len(current) > 0:
+          urls.append(url+current)
+        addElement = False
+        current=''
+      i += 1
+  return urls
+
+  #======================================
+
+client = discord.Client()
+TOKEN = os.environ['TOKEN'] # config('TOKEN') -> se estiver no computador
+
+voice = None
+songsList = []
+
+#https://www.instagram.com/p/ - MUSCLE
+#https://youtu.be/ - MUSCLE
+#https://gfycat.com/ - MUSCLE E BOOBS
+#https://redgifs.com/watch/ - BOOBS
+boobsList = getPhoto('imgStorage/BOOBS', 'https://i.imgur.com/')
+muscleList = getPhoto('imgStorage/MUSCLE', 'https://i.imgur.com/') + getPhoto('imgStorage/MUSCLE', 'https://youtu.be/') + getPhoto('imgStorage/MUSCLE', 'https://www.instagram.com/p/')
+photoList = [boobsList, muscleList]
 
 @client.event
 async def on_ready():
@@ -91,8 +132,55 @@ async def on_message(message):
   elif message.author.voice == None and message.content.startswith('-leave'):
       await channel.send("Tu não ta on no voice")
 #Boobs
+  elif message.content.startswith('-sena'):
+    rand = randint(0,1)
+    currentList = photoList[rand]
+    if rand == 0:
+      photo = currentList[randint(0,len(boobsList)-1)]
+    else:
+      photo = currentList[randint(0,len(muscleList)-1)]
+    await channel.send(photo)
   #elif message.content.startswith('-sena'):
+# Plotagem
+  elif message.content.startswith('-plota '):
+    ent = message.content.split()
+    funcs = []
+    x = []
+    y = []
+    for index in range(1, len(ent)):
+        p = ent[index].split(',')
+        px = float(p[0][1:])
+        py = float(p[1][:len(p[1])-1])
+        x.append(px)
+        y.append(py)
+    #Envia uma equação do 1 grau
+    for i in range(len(x)-1):
+        funcs.append(f'y = {(y[i+1]-y[i])}/{(x[i+1]-x[i])}*x + ({(-(y[i+1]-y[i])*x[i])+(y[i]*(x[i+1]-x[i]))}/{(x[i+1]-x[i])})')
+    for i in funcs:
+      await channel.send(i)
     
+    plt.plot(x,y)
 
+    plt.xlabel('x values')
+    plt.ylabel('y values')
+    plt.title('plotted x and y values')
+    plt.legend(['line 1'])
+
+    plt.savefig('plotting/plot.png', dpi=300, bbox_inches='tight')
+    plt.clf() 
+
+    client_id = '298648bf6f7a17b'
+    path = 'plotting/plot.png'
+    im = pyimgur.Imgur(client_id)
+    upload_image = im.upload_image(path, title='Plot')
+
+    os.remove('plotting/plot.png')
+
+    await channel.send(upload_image.link)
+
+  # Realiza Calculo
+  elif message.content.startswith('-uga '):
+      calc = message.content[len('-uga '):]
+      await channel.send(eval(calc))
 
 client.run(TOKEN)
